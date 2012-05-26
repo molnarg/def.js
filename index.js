155d03653b506dc613b2b2a61c2ac8e783ad29f1
Gábor Molnár <gabor.molnar@sch.bme.hu>
2012-05-26 22:27:40 +0200

    Complete rewrite.

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var def = require('./lib/def')
-  , i
-
-module.exports = function() {
-  var fn = def.Function()
-
-  return fn.def.apply(fn, arguments)
-}
-
-for (i in def) module.exports[i] = def[i]
+module.exports = require('./lib/def')
